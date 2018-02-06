d8a5a1677e75608e6420e038dca4bda0540df93a
EC2 Default User <ec2-user@ip-172-31-27-112.us-east-2.compute.internal>
2018-02-06 22:53:12 +0000

    fixed errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,12 +1,11 @@
-require 'rails_helper'
+ require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-    # creating a new instance of the post class and computes and stoes the returned value when called in the it block
-    let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
-    describe attributes do
-        # tests whether post has attributes
-        it "has title and body attributes" do
-            expect(post).to have_attributes(title: "New Post Title", body: "New Post Body")
-        end
-    end
-end
+ RSpec.describe Post, type: :model do
+   let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
+
+   describe "attributes" do
+     it "has title and body attributes" do
+       expect(post).to have_attributes(title: "New Post Title", body: "New Post Body")
+     end
+   end
+ end
