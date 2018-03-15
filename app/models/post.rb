b419e0abf4c65b7ddfa931f9f57bba2cc4159f33
EC2 Default User <ec2-user@ip-172-31-27-112.us-east-2.compute.internal>
2018-03-15 20:04:33 +0000

    added two post ordering scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     # order post by their created_at date in decending order
     default_scope { order('created_at DESC') }
+    scope :ordered_by_title, -> { order('title DESC') }
+    scope :ordered_by_reversed_created_at, -> { order('created_at ASC') }
     
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
